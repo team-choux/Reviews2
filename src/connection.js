bc8cce5f46ee4c4199b6116f4af4105de02c54dc
chaomonica <alittlefaun@gmail.com>
2020-12-02 20:00:42 -0600

    established connection with database

---
+++ src/connection.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+//opens a connection to the reviewsDB database on our locally running instace of MongoDB
+mongoose.connect('mongodb://localhost/reviewsDB', {
+  useUnifiedTopology: true,
+  useNewUrlParser:true});
+
+
+mongoose.connection
+//notifies on connection error
+.on('error', function(error){
+  console.log('Connection error: ', error)
+})
+//notifies on successful connection
+.once('open', function() {
+  console.log('connection has been made');
+})
