2d78d4856a92af0593c0fe7ab9202491b5ce918d
chaomonica <alittlefaun@gmail.com>
2020-12-07 10:31:10 -0600

    set up api routes and created method to quickly populate meta table

--- SDC/server/index.js
+++ SDC/server/index.js
@@ -1,19 +1,30 @@
+/*
+https://stackoverflow.com/questions/36002413/conventions-for-app-js-index-js-and-server-js-in-node-js
+Naming conventions on entry points of applications:
+index.js - npm init sets the main entry point of the module to index.js
+server.js - if no start script is supplied, npm start will by default run "node server.js"
+app.js - some IDEs (integrated development environment) such as VS Code will default to app.js as the entry point of a program you debug
+
+*/
+
 const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('../database-mysql');
-
+const routes = require('./routes.js');
 const app = express();
-const PORT = 3000;
+/*
+https://expressjs.com/en/guide/routing.html
+Use the express.Router class to create modular, mountable route handlers.
+A Router instance is a complete middleware and routing system;
+for this reason, it is often referred to as a “mini-app”.
+*/
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('./react-client/dist'));
+const port = 3000;
 
 
-// UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use('/', routes);
 
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
+
+
